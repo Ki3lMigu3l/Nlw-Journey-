2f62cc7b7aeeb825058d1712a592433bd5e9e0d0
Ezequiel <ki3lmigu3l@gmail.com>
2024-07-09 21:42:35 -0300

    Update no projeto

---
+++ journey/src/main/java/br/com/rockectseat/journey/trip/TripCreateResponse.java
@@ -0,0 +1,6 @@
+package br.com.rockectseat.journey.trip;
+
+import java.util.UUID;
+
+public record TripCreateResponse (UUID tripId) {
+}
---
+++ journey/src/main/java/br/com/rockectseat/journey/trip/TripRepository.java
@@ -0,0 +1,9 @@
+package br.com.rockectseat.journey.trip;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TripRepository extends JpaRepository<Trip, UUID> {
+
+}
